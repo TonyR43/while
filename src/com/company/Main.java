7e9cdb2e341fb1fb90f2a966482604646ae263c3
Tony <tony.ratley430@gmail.com>
2019-02-07 21:19:10 -0600

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main
+    {
+        
+        public static void main(String[] args)
+            {
+                int num = 100;
+                while (num > 0)
+                {
+                    System.out.println("While Countdown: " + num);
+                    num-=10;
+                }
+                
+                
+            }
+    }
